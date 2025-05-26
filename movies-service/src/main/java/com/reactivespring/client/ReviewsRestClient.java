98fd6f447678cb479fd3ec408d7874be31adcbad
PAULO FARIA <prfitsolutionsbe@gmail.com>
2025-05-26 14:49:00 +0200

    Creating integration part of Movie Service

--- movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
@@ -1,6 +1,5 @@
 package com.reactivespring.client;
 
-import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.domain.Review;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
