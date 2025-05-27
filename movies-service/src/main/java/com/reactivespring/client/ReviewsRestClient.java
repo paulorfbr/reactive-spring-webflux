c3f08e90f6cd8a242fb7bb6ee87791f4426e07c2
PAULO FARIA <prfitsolutionsbe@gmail.com>
2025-05-27 10:00:02 +0200

    Adding custom exception handling for the Rest clients

--- movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
@@ -1,13 +1,21 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.Review;
+import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
+import com.reactivespring.exception.ReviewsClientException;
+import com.reactivespring.exception.ReviewsServerException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class ReviewsRestClient {
     private WebClient webClient;
 
@@ -28,6 +36,23 @@ public class ReviewsRestClient {
                 .get()
                 .uri(url)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                    log.info("Status code is {}", clientResponse.statusCode().value());
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)){
+                        return Mono.empty(); //no need for exception, just return empty list
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage ->
+                                    Mono.error(new ReviewsClientException(responseMessage))
+                            );
+                })
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+                    log.info("Status code is {}", clientResponse.statusCode().value());
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage ->
+                                    Mono.error(new ReviewsServerException("ServerException in Reviews service" + responseMessage))
+                            );
+                })
                 .bodyToFlux(Review.class)
                 .log();
     }
